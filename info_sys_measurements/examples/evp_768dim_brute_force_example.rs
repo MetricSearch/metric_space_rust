a1493517b0cf2924d7bee183ca2669f7d4c1e95f
pchire <ben.claydon2000@gmail.com>
2026-02-18 14:24:29 +0000

    Fix 768d EVP

--- info_sys_measurements/examples/evp_768dim_brute_force_example.rs
+++ info_sys_measurements/examples/evp_768dim_brute_force_example.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 //use std::random::random;
-use bits::container::Simd256x2;
+use bits::container::Simd256x4;
 use bits::{distance, EvpBits};
 use ndarray::Array1;
 use rand::random;
@@ -14,11 +14,11 @@ fn main() -> Result<()> {
     let dims = 768;
 
     let query_f32 = Array1::from_iter((0..dims).map(|_| random::<f32>()));
-    let query: EvpBits<Simd256x2, 768> = EvpBits::<Simd256x2, 768>::from_embedding(query_f32, 420);
+    let query: EvpBits<Simd256x4, 768> = EvpBits::<Simd256x4, 768>::from_embedding(query_f32, 420);
 
-    let data: Array1<EvpBits<Simd256x2, 768>> = Array1::from_iter((0..num_data).map(|_| {
+    let data: Array1<EvpBits<Simd256x4, 768>> = Array1::from_iter((0..num_data).map(|_| {
         let embedding = Array1::from_iter((0..dims).map(|_| random::<f32>()));
-        EvpBits::<Simd256x2, 768>::from_embedding(embedding, 420)
+        EvpBits::<Simd256x4, 768>::from_embedding(embedding, 420)
     }));
 
     let now = Instant::now();
@@ -37,8 +37,8 @@ fn main() -> Result<()> {
 }
 
 fn generate_bsp_dists(
-    query: EvpBits<Simd256x2, 768>,
-    data: Array1<EvpBits<Simd256x2, 768>>,
+    query: EvpBits<Simd256x4, 768>,
+    data: Array1<EvpBits<Simd256x4, 768>>,
 ) -> Vec<usize> {
     data.iter()
         .map(|datum| distance(&query, &datum))

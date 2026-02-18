ad60eaf97713ab156aa068f8578b83b8e96ee810
pchire <ben.claydon2000@gmail.com>
2026-02-18 11:58:36 +0000

    Update shell file for infosys experiments

--- run_infosys_experiments.sh
+++ run_infosys_experiments.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-cargo run -r --example evp_100dim_brute_force_example
-cargo run -r --example evp_384dim_brute_force_example
-cargo run -r --example evp_500dim_brute_force_example
-cargo run -r --example evp_768dim_brute_force_example 
 
-cargo run -r --example 4bit_brute_force_example      
-cargo run -r --example 8bit_brute_force_example
-cargo run -r --example 16bit_brute_force_example         
+for LOOP in {1..10}
+do
+    {
+    cargo run -r --example evp_100dim_brute_force_example 
+    cargo run -r --example evp_384dim_brute_force_example
+    cargo run -r --example evp_500dim_brute_force_example
+    cargo run -r --example evp_768dim_brute_force_example 
 
-cargo run -r --example f32_euc_brute_force_example
+    cargo run -r --example 4bit_brute_force_example      
+    cargo run -r --example 8bit_brute_force_example
+    cargo run -r --example 16bit_brute_force_example         
 
-cargo run -r --example hamming_100dim_brute_force_example
-cargo run -r --example hamming_384dim_brute_force_example
-cargo run -r --example hamming_500dim_brute_force_example
-cargo run -r --example hamming_768dim_brute_force_example
+    cargo run -r --example f32_euc_brute_force_example
+
+    cargo run -r --example hamming_100dim_brute_force_example
+    cargo run -r --example hamming_384dim_brute_force_example
+    cargo run -r --example hamming_500dim_brute_force_example
+    cargo run -r --example hamming_768dim_brute_force_example
+    } > infosys_raw_data/$LOOP.txt
+done
